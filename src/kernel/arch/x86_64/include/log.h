d7fa68c2fbd4697589fba5c4a069282d7736567b
LinChenjun2008 <linchenjun2008@outlook.com>
2024-02-04 19:48:10 +0800

    chore: update

--- src/kernel/arch/x86_64/include/log.h
+++ src/kernel/arch/x86_64/include/log.h
@@ -10,6 +10,50 @@ PUBLIC void basic_put_char(char c,uint32_t col);
 PUBLIC void basic_print(uint32_t col,const char *str,...);
 
 #if __DISABLE_LOG__
+static inline void pr_log(const char *log,...){(void)log;}
+#elif !__DISABLE_LOG__
+
+#define IS_TRANSMIT_EMPTY(port) (io_in8(port + 5) & 0x20)
+
+static inline void serial_pr_log(const char *log,va_list ap)
+{
+    char msg[256];
+    char *buf;
+    uint16_t port = 0x3f8;
+    char *level[] =
+    {
+        "\033[32m[ INFO  ]\033[0m ",
+        "\033[33m[ DEBUG ]\033[0m ",
+        "\033[31m[ ERROR ]\033[0m ",
+    };
+    if (*log >= 1 && *log <= 3)
+    {
+        buf = level[*log - 1];
+        while (IS_TRANSMIT_EMPTY(port) == 0);
+        if(*buf != 0)
+        {
+            do
+            {
+                while (IS_TRANSMIT_EMPTY(port) == 0);
+                io_out8(port,*buf);
+            } while (*buf++);
+        }
+    }
+    vsprintf(msg,log,ap);
+    buf = msg;
+    while (IS_TRANSMIT_EMPTY(port) == 0);
+    if(*buf != 0)
+    {
+        do
+        {
+            while (IS_TRANSMIT_EMPTY(port) == 0);
+            io_out8(port,*buf);
+        } while (*buf++);
+    }
+    return;
+}
+#undef IS_TRANSMIT_EMPTY
+
 static inline void pr_log(const char *log,...)
 {
     char msg[256];
@@ -41,58 +85,15 @@ static inline void pr_log(const char *log,...)
     vsprintf(msg,log,ap);
     buf = msg;
     basic_print(0x00ffffff,buf);
-    return;
-}
-#else
-#define IS_TRANSMIT_EMPTY(port) (io_in8(port + 5) & 0x20)
 
-static inline void pr_log(const char *log,...)
-{
-    char msg[256];
-    char *buf;
-    uint16_t port = 0x3f8;
-    char *level[] =
-    {
-        "\033[32m[ INFO  ]\033[0m ",
-        "\033[33m[ DEBUG ]\033[0m ",
-        "\033[31m[ ERROR ]\033[0m ",
-        "[ INFO  ] ",
-        "[ DEBUG ] ",
-        "[ ERROR ] "
-    };
-    if (*log >= 1 && *log <= 3)
-    {
-        buf = level[*log - 1];
-        while (IS_TRANSMIT_EMPTY(port) == 0);
-        if(*buf != 0)
-        {
-            do
-            {
-                while (IS_TRANSMIT_EMPTY(port) == 0);
-                io_out8(port,*buf);
-            } while (*buf++);
-        }
-        buf = level[*log + 2];
-        basic_print(0x0000ff00,buf);
-    }
-    va_list ap;
+    #if !__DISABLE_SERIAL_LOG__
     va_start(ap,log);
-    vsprintf(msg,log,ap);
-    buf = msg;
-    while (IS_TRANSMIT_EMPTY(port) == 0);
-    if(*buf != 0)
-    {
-        do
-        {
-            while (IS_TRANSMIT_EMPTY(port) == 0);
-            io_out8(port,*buf);
-        } while (*buf++);
-    }
-    buf = msg;
-    basic_print(0x00ffffff,buf);
+    serial_pr_log(log,ap);
+    #endif
+
     return;
 }
-#undef IS_TRANSMIT_EMPTY
+
 #endif
 
 #endif
