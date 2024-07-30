9601b33a928612da090bd7dd20cdb4495136ba1a
LinChenjun2008 <linchenjun2008@outlook.com>
2024-07-30 16:27:13 +0800

    feat: VGA font

--- src/kernel/graphic/print.c
+++ src/kernel/graphic/print.c
@@ -24,29 +24,27 @@ PRIVATE position_t pos = {X_START,Y_START};
 
 PUBLIC void basic_put_char(char c,uint32_t col)
 {
-    if (c >= ' ' && c <= '~')
+    uint8_t *character = ascii_character[(uint32_t)c];
+    int i;
+    for (i = 0;i < 16;i++)
     {
-        uint8_t *character = ascii_character[c - ' '];
-        int i;
-        for (i = 0;i < 16;i++)
-        {
-            uint8_t data = character[i];
-            uint32_t *pixel = \
-                (uint32_t*)g_boot_info->graph_info.frame_buffer_base \
-                + (pos.y + i) * g_boot_info->graph_info.horizontal_resolution \
-                + pos.x;
-            int j;
-            for (j = 0;j < 8;j++){ pixel[j] = 0x00000000; }
-            if ((data & 0x80) != 0){ pixel[0] = col; }
-            if ((data & 0x40) != 0){ pixel[1] = col; }
-            if ((data & 0x20) != 0){ pixel[2] = col; }
-            if ((data & 0x10) != 0){ pixel[3] = col; }
-            if ((data & 0x08) != 0){ pixel[4] = col; }
-            if ((data & 0x04) != 0){ pixel[5] = col; }
-            if ((data & 0x02) != 0){ pixel[6] = col; }
-            if ((data & 0x01) != 0){ pixel[7] = col; }
-        }
+        uint8_t data = character[i];
+        uint32_t *pixel = \
+            (uint32_t*)g_boot_info->graph_info.frame_buffer_base \
+            + (pos.y + i) * g_boot_info->graph_info.horizontal_resolution \
+            + pos.x;
+        int j;
+        for (j = 0;j < 8;j++){ pixel[j] = 0x00000000; }
+        if ((data & 0x80) != 0){ pixel[0] = col; }
+        if ((data & 0x40) != 0){ pixel[1] = col; }
+        if ((data & 0x20) != 0){ pixel[2] = col; }
+        if ((data & 0x10) != 0){ pixel[3] = col; }
+        if ((data & 0x08) != 0){ pixel[4] = col; }
+        if ((data & 0x04) != 0){ pixel[5] = col; }
+        if ((data & 0x02) != 0){ pixel[6] = col; }
+        if ((data & 0x01) != 0){ pixel[7] = col; }
     }
+
     return;
 }
 
