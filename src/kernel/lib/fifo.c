ba9da094e4574bc98c98675041736eb565d8eb36
linchenjun <linchenjun2008@outlook.com>
2024-08-30 13:54:11 +0800

    add lincense (#18)

--- src/kernel/lib/fifo.c
+++ src/kernel/lib/fifo.c
@@ -1,5 +1,34 @@
+/*
+   Copyright 2024 LinChenjun
+
+This file is part of Clay Figure Kernel.
+
+Clay Figure Kernel is free software: you can redistribute it and/or modify
+it underthe terms of the GNU General Public License as published by
+the Free Software Foundation,either version 3 of the License, or (at your option)
+any later version.
+
+Clay Figure Kernel is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY;without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Clay Figure Kernel.If not, see
+<https://www.gnu.org/licenses/>.
+
+本文件是Clay Figure Kernel的一部分。
+
+Clay Figure Kernel 是自由软件：你可以再分发之和/或依照由自由软件基金会发布的
+GNU 通用公共许可证修改之，无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。
+
+发布 Clay Figure Kernel 是希望它能有用，但是并无保障;
+甚至连可销售和符合某个特定的目的都不保证。请参看GNU 通用公共许可证，了解详情。
+
+你应该随程序获得一份 GNU 通用公共许可证的复本。如果没有，请看
+<https://www.gnu.org/licenses/>。  */
+
 #include <kernel/global.h>
-// #include <intr.h>
 #include <lib/fifo.h>
 
 PUBLIC void init_fifo(fifo_t *fifo,void *buf,int type,int size)
@@ -33,7 +62,6 @@ PUBLIC status_t fifo_put(fifo_t *fifo,void* data)
     {
         return K_ERROR;
     }
-    // intr_status_t intr_status = intr_disable();
 
     if (fifo->free == 0) /* 没有空余 */
     {
@@ -56,7 +84,6 @@ PUBLIC status_t fifo_put(fifo_t *fifo,void* data)
             break;
     }
     fifo->nw = (fifo->nw + 1) % fifo->size;
-    // intr_set_status(intr_status);
     return K_SUCCESS;
 }
 
@@ -66,7 +93,6 @@ PUBLIC status_t fifo_get(fifo_t *fifo,void* data)
     {
         return K_ERROR;
     }
-    // intr_status_t intr_status = intr_disable();
     if (fifo->free == fifo->size)
     {
         return K_ERROR;
@@ -88,7 +114,6 @@ PUBLIC status_t fifo_get(fifo_t *fifo,void* data)
             break;
     }
     fifo->nr = (fifo->nr + 1) % fifo->size;
-    // intr_set_status(intr_status);
     return K_SUCCESS;
 }
 
