345f8bc5738e601bf833f1cc6d9e306ef48a348b
linchenjun <linchenjun2008@outlook.com>
2024-11-27 17:15:09 +0800

    update stdio.c

--- src/lib/stdio.c
+++ src/lib/stdio.c
@@ -32,14 +32,14 @@ GNU 宽通用公共许可证修改之，无论是版本 3 许可证，还是（�
 #include <std/string.h>
 #include <std/stdarg.h>
 
-#define FORMAT_SPACE   0
+
 #define FORMAT_LEFT    (1 << 0)
-#define FORMAT_ZERO    (1 << 1)
-#define FORMAT_SIGN    (1 << 2)
-#define FORMAT_SPECIAL (1 << 3)
-#define FORMAT_SMALL   (1 << 4)
-#define FORMAT_LONG    (1 << 5)
-#define FORMAT_SHORT   (1 << 6)
+#define FORMAT_SPACE   (1 << 1)
+#define FORMAT_ZERO    (1 << 2)
+#define FORMAT_SIGN    (1 << 3)
+#define FORMAT_PLUS    (1 << 4)
+#define FORMAT_SPECIAL (1 << 5)
+#define FORMAT_SMALL   (1 << 6)
 
 #define IS_DIGIT(c) ((c) >= '0' && (c) <= '9')
 
@@ -53,85 +53,68 @@ PRIVATE int skip_atoi(const char **s)
     return i;
 }
 
-PRIVATE void signed_int_to_string(int64_t a,char* str,int base,int small)
+PRIVATE char * number_to_string(char * str,uint64_t num,int base,int width,int precision,int flag)
 {
-    const char *digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    if (small)
+	char pad,sign,tmp[50];
+	const char *digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	if (flag & FORMAT_SMALL)
     {
         digits = "0123456789abcdefghijklmnopqrstuvwxyz";
     }
-    int i;
-    int is_negative;
-    is_negative = a;
-    if (a < 0)
+	if (base < 2 || base > 36)
+		return 0;
+	pad = (flag & FORMAT_ZERO) ? '0' : ' ' ;
+	sign = 0;
+	if (flag & FORMAT_SIGN && (int64_t)num < 0)
     {
-        a = -a;
+		sign = '-';
+		num = -num;
+	} 
+    else
+	{
+        sign=(flag & FORMAT_PLUS) ? '+' : ((flag & FORMAT_SPACE) ? ' ' : 0);
     }
-    i = 0;
-    do
-    {
-        str[i] = digits[a % base];
-        i++;
-        a = a / base;
-    } while (a > 0);
-    if (is_negative < 0)
-    {
-        str[i] = '-';
-        i++;
+	if (sign) width--;
+	if (flag & FORMAT_SPECIAL)
+	{
+        if (base == 16) width -= 2;
+		if (base ==  8) width -= 1;
     }
-    str[i] = '\0';
-    char* p = str;
-    char* q = str;
-    char tmp;
-    while (*q != '\0')
+	int i = 0;
+	if (num == 0) { tmp[i++]='0'; }
+	else 
     {
-        q++;
+        while (num != 0)
+        {
+            tmp[i++]=digits[num % base];
+            num /= base;
+        }
     }
-    q--;
-    while (q > p)
+	if (i > precision) { precision=i; }
+	width -= precision;
+	if (!(flag & (FORMAT_ZERO | FORMAT_LEFT)))
+	{
+        while(width-- > 0) *str++ = ' ';
+    }
+	if (sign) { *str++ = sign; }
+	if (flag & FORMAT_SPECIAL)
+	{
+        if (base == 8) { *str++ = '0'; }
+		if (base==16) 
+		{
+			*str++ = '0';
+			*str++ = digits[33];
+		}
+    }
+	if (!(flag & FORMAT_LEFT))
     {
-        tmp = *p;
-        *p = *q;
-        p++;
-        *q = tmp;
-        q--;
+        while(width-- > 0) *str++ = pad;
     }
-    return;
-}
 
-PRIVATE void unsigned_int_to_string(uint64_t a,char* str,int base,int small)
-{
-    const char *digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    if (small)
-    {
-        digits = "0123456789abcdefghijklmnopqrstuvwxyz";
-    }
-    int i;
-    i = 0;
-    do
-    {
-        str[i] = digits[a % base];
-        i++;
-        a = a / base;
-    } while (a > 0);
-    str[i] = '\0';
-    char* p = str;
-    char* q = str;
-    char tmp;
-    while (*q != '\0')
-    {
-        q++;
-    }
-    q--;
-    while (q > p)
-    {
-        tmp = *p;
-        *p = *q;
-        p++;
-        *q = tmp;
-        q--;
-    }
-    return;
+	while(i < precision--) *str++ = '0';
+	while(i-- > 0) *str++ = tmp[i];
+	while(width-- > 0) *str++ = ' ';
+	return str;
 }
 
 PRIVATE int get_flag(const char **fmt)
@@ -150,28 +133,23 @@ PRIVATE int get_flag(const char **fmt)
         flag |= FORMAT_ZERO;
         goto repeat;
     case ' ':
+        flag |= FORMAT_SPACE;
         goto repeat;
         break;
     case '#':
         flag |= FORMAT_SPECIAL;
         goto repeat;
         break;
-    case 'l':
-        flag |= FORMAT_LONG;
-        goto repeat;
-    case 'h':
-        flag |= FORMAT_SHORT;
-        goto repeat;
     default:
         break;
     }
-    if (flag & FORMAT_LEFT) { flag &= ~FORMAT_LEFT; }
+    if (flag & FORMAT_LEFT) { flag &= ~FORMAT_ZERO; }
     return flag;
 }
 
 PUBLIC int vsprintf(char *buf,const char *fmt,va_list ap)
 {
-    char* str,*s = 0,digits[69],qualifier;
+    char* str,*s,qualifier;
     int flag;
     int width;
     for (str = buf;*fmt != '\0';fmt++)
@@ -181,7 +159,6 @@ PUBLIC int vsprintf(char *buf,const char *fmt,va_list ap)
             *str++ = *fmt;
             continue;
         }
-        digits[0] = 0;
         flag = get_flag(&fmt);
 
         width = -1;
@@ -201,6 +178,7 @@ PUBLIC int vsprintf(char *buf,const char *fmt,va_list ap)
             *str = '%';
             str++;
             break;
+
         case 'c':/* %c */
             if (!(flag & FORMAT_LEFT))
             {
@@ -209,91 +187,62 @@ PUBLIC int vsprintf(char *buf,const char *fmt,va_list ap)
             *str++ = va_arg(ap,uint64_t);
             while (--width > 0) {*str++ = ' ';}
             break;
+
         case 'd': /* %d */
         case 'i':
-            s = digits;
-            signed_int_to_string(va_arg(ap,uint64_t),s,10,0);
-            break;
-        case 'o': /* %o */
-            s = digits;
+            flag |= FORMAT_SIGN;
+        case 'u':
             if (qualifier == 'l')
             {
-                unsigned_int_to_string(va_arg(ap,uint64_t),s,8,0);
+                str = number_to_string(str,va_arg(ap,long long int),10,width,-1,flag);
             }
             else
             {
-                unsigned_int_to_string(va_arg(ap,int),s,8,0);
+                str = number_to_string(str,va_arg(ap,int),10,width,-1,flag);
             }
             break;
-        case 'p':
-            s = digits;
-            unsigned_int_to_string((addr_t)va_arg(ap,addr_t),digits,16,0);
+
+        case 'o': /* %o */
+            if (qualifier == 'l')
+            {
+                str = number_to_string(str,va_arg(ap,long long int),8,width,-1,flag);
+            }
+            else
+            {
+                str = number_to_string(str,va_arg(ap,int),8,width,-1,flag);
+            }
             break;
+
+        case 'p':
+            width = 2 * sizeof(void*);
+            flag |= FORMAT_ZERO;
+            str = number_to_string(str,va_arg(ap,addr_t),16,width,16,flag);
+            break;
+
         case 's': /* %s */
             s = va_arg(ap,char*);
             strcpy(str,s);
+            str += strlen(s);
             break;
-        case 'u': /* %u */
-            s = digits;
-            unsigned_int_to_string(va_arg(ap,uint64_t),s,10,0);
-            break;
+
         case 'x': /* %x */
-            s = digits;
-            if (qualifier == 'l')
-            {
-                unsigned_int_to_string(va_arg(ap,uint64_t),s,16,1);
-            }
-            else
-            {
-                unsigned_int_to_string(va_arg(ap,int),s,16,1);
-            }
-            break;
+            flag |= FORMAT_SMALL;
         case 'X':
-            s = digits;
             if (qualifier == 'l')
             {
-                unsigned_int_to_string(va_arg(ap,uint64_t),s,16,1);
+                str = number_to_string(str,va_arg(ap,long long int),16,width,-1,flag);
             }
             else
             {
-                unsigned_int_to_string(va_arg(ap,int),s,16,1);
+                str = number_to_string(str,va_arg(ap,int),16,width,-1,flag);
             }
-            unsigned_int_to_string(va_arg(ap,uint64_t),s,16,0);
             break;
+
         default:
             *str++ = '%';
             if(*fmt) { *str++ = *fmt; }
             else     { fmt--; }
             break;
-        }
-        width -= strlen(s);
-        if (flag & FORMAT_SPECIAL)
-        {
-            width -= 2;
-        }
-        if ((flag & FORMAT_SPECIAL) && (flag & FORMAT_ZERO))
-        {
-            *str++ = '0';
-            *str++ = 'x';
-        }
-        while (width > 0 && !(flag & FORMAT_LEFT))
-        {
-            *str++ = (flag & FORMAT_ZERO ? '0' : ' ');
-            width--;
-        }
-        if ((flag & FORMAT_SPECIAL) && !(flag & FORMAT_ZERO))
-        {
-            *str++ = '0';
-            *str++ = 'x';
-        }
-        strcpy(str,s);
-        str += strlen(s);
-        /* 左对齐的情况 */
-        while (width > 0 && flag & FORMAT_LEFT)
-        {
-            *str = ' ';
-            str++;
-            width--;
         }
     }
     *str = '\0';
