8400b7146bfd82cf1c99f9515232ef1635a18f8a
linchenjun <linchenjun2008@outlook.com>
2024-12-04 17:19:14 +0800

    修改版权声明 (#29)
    * update 2024.11.27
    
    * update 2024.12.02
    
    * update 2024.12.04

--- src/arch/x86_64/include/kernel/def.h
+++ src/arch/x86_64/include/kernel/def.h
@@ -1,32 +1,10 @@
 /*
    Copyright 2024 LinChenjun
 
-This file is part of Clay Figure Kernel.
+   本程序是自由软件
+   修改和/或再分发依照 GNU GPL version 3 (or any later version)
 
-Clay Figure Kernel is free software: you can redistribute it and/or modify
-it underthe terms of the GNU Lesser General Public License as published by
-the Free Software Foundation,either version 3 of the License, or (at your option)
-any later version.
-
-Clay Figure Kernel is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY;without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with Clay Figure Kernel.If not, see
-<https://www.gnu.org/licenses/>.
-
-本文件是Clay Figure Kernel的一部分。
-
-Clay Figure Kernel 是自由软件：你可以再分发之和/或依照由自由软件基金会发布的
-GNU 宽通用公共许可证修改之，无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。
-
-发布 Clay Figure Kernel 是希望它能有用，但是并无保障;
-甚至连可销售和符合某个特定的目的都不保证。请参看GNU 宽通用公共许可证，了解详情。
-
-你应该随程序获得一份 GNU 宽通用公共许可证的复本。如果没有，请看
-<https://www.gnu.org/licenses/>。  */
+*/
 
 #ifndef __DEF_H__
 #define __DEF_H__
