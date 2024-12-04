e6d8b546a24012bc3ea0f01a216a16f96475c6e4
linchenjun <linchenjun2008@outlook.com>
2024-12-04 17:17:48 +0800

    update 2024.12.04

--- src/arch/x86_64/include/device/pci.h
+++ src/arch/x86_64/include/device/pci.h
@@ -1,9 +1,9 @@
 /*
    Copyright 2024 LinChenjun
 
-   本文件是Clay Figure Kernel的一部分。
-   修改和/或再分发遵循GNU GPL version 3 (or any later version)
-  
+   本程序是自由软件
+   修改和/或再分发依照 GNU GPL version 3 (or any later version)
+
 */
 
 #ifndef __PCI_H__
