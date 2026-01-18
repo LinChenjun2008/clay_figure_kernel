e6a6d570104bc8ce7b4a25095a1212e939c2716e
linchenjun <linchenjun2008@outlook.com>
2026-01-18 14:34:33 +0800

    using abspath

--- scripts/target.mk
+++ scripts/target.mk
@@ -1,8 +1,8 @@
-RUNNING_DIR       = $(PROJECT_DIR)/../run
-ESP_DIR           = $(RUNNING_DIR)/esp
-TARGET_BOOTLOADER = $(ESP_DIR)/EFI/Boot/BootX64.efi
-TARGET_KERNEL     = $(ESP_DIR)/Kernel/clfgkrnl.sys
-TARGET_INITRAMFS  = $(ESP_DIR)/Kernel/initramfs.img
+RUNNING_DIR       = $(abspath $(PROJECT_DIR)/../run)
+ESP_DIR           = $(abspath $(RUNNING_DIR)/esp)
+TARGET_BOOTLOADER = $(abspath $(ESP_DIR)/EFI/Boot/BootX64.efi)
+TARGET_KERNEL     = $(abspath $(ESP_DIR)/Kernel/clfgkrnl.sys)
+TARGET_INITRAMFS  = $(abspath $(ESP_DIR)/Kernel/initramfs.img)
 
 TARGET_ARCH       = x86_64
 SMP_CORES         = 4
