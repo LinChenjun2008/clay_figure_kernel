ab61231a958d34118f47bab2cc4152a65a5a4aa1
linchenjun <linchenjun2008@outlook.com>
2026-08-18 22:33:17 +0800

    update main.c

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -7,29 +7,15 @@
 
 #include <asm/init.h>
 
-// TEST
-#include <print.h>
-#include <syscall.h>
 #include <task.h>
-
-static int proc()
-{
-    return 0;
-}
 
 int main(struct boot_info *boot_info)
 {
     init_all(boot_info);
-    process_execute("process", DEFAULT_PRIO, 1, 1, proc);
 
-    int status = 0;
     while (1)
     {
-        pid_t exited = task_waitpid(-1, &status, WNOHANG);
-        if (exited > 0)
-        {
-            printk("task %d exit with status: %d.\n", exited, status);
-        }
+        task_waitpid(-1, NULL, WNOHANG);
     }
     return 0;
 }
@@ -37,16 +23,10 @@ int main(struct boot_info *boot_info)
 int ap_main(uintptr_t stack)
 {
     ap_init_all(stack);
-    process_execute("process", DEFAULT_PRIO, 1, 1, proc);
 
-    int status = 0;
     while (1)
     {
-        pid_t exited = task_waitpid(-1, &status, WNOHANG);
-        if (exited > 0)
-        {
-            printk("task %d exit with status: %d.\n", exited, status);
-        }
+        task_waitpid(-1, NULL, WNOHANG);
     }
     return 0;
 }
